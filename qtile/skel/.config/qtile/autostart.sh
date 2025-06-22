85818b43649fcce8441d76fedbe9760f79770a15
Daniel Azevedo <daniazevedo77@posteo.net>
2025-06-23 00:12:27 +0100

    xmonad_install

--- qtile/skel/.config/qtile/autostart.sh
+++ qtile/skel/.config/qtile/autostart.sh
@@ -7,3 +7,6 @@ udiskie -a &
 xcompmgr -c -f -n &
 xautolock -time 5 -locker slock &
 redshift -l 41.6:-8.62 -t 5700:3500 -b 0.8 &
+synclient TapButton1=1 &
+synclient TapButton2=3 &
+synclient TapButton3=2 &
