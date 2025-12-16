40c3ef14ed0e0e6dca9f87650cda56767dec7dc9
Daniel Azevedo <daniazevedo77@posteo.net>
2025-12-16 16:31:13 +0000

    alias and calamares/d77 repositories sorting

--- extras/d77.aliases.bash
+++ extras/d77.aliases.bash
@@ -12,11 +12,13 @@ function _set_pkg_aliases() {
 		alias pkginst='sudo xbps-install -S'
 		alias pkgupg='sudo xbps-install -Syu'
 		alias pkgrm='sudo xbps-remove -R'
-		alias pkgsig='xbps-rindex --sign-pkg --privkey privkey.pem hostdir/binpkgs/*.xbps'
+		alias pkgsig='xbps-rindex --sign-pkg --privkey privkey.pem hostdir/binpkgs/d77/*.xbps'
+		alias calasig='xbps-rindex --sign-pkg --privkey privkey.pem hostdir/binpkgs/*.xbps'		
 
 		alias srcbuild='./xbps-src pkg'
-		alias isobuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/calamares/'
-		alias hyprbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/calamares/ -r https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc'
+		alias isobuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/'
+		alias hyprbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc'
+		alias cosmicbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://bellawagner.de/repo/x86_64 -b cosmic -- -T d77void'
 	fi
 }
 
