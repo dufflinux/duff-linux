9b2e3d9761c7d20f8c592181054e6faaf93d69e6
Daniel Azevedo <daniazevedo77@posteo.net>
2026-01-01 23:54:37 +0000

    d77aliases: added klist & krem to list and remove kernels

--- extras/d77.aliases.bash
+++ extras/d77.aliases.bash
@@ -14,8 +14,11 @@ function _set_pkg_aliases() {
 		alias pkgrm='sudo xbps-remove -R'
 		alias pkgsig='xbps-rindex --sign-pkg --privkey privkey.pem hostdir/binpkgs/d77/*.xbps'
 		alias calasig='xbps-rindex --sign-pkg --privkey privkey.pem hostdir/binpkgs/*.xbps'		
-
 		alias srcbuild='./xbps-src pkg'
+
+		alias klist='vkpurge list'
+		alias krem='sudo vkpurge'
+
 		alias isobuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/'
 		alias hyprbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://raw.githubusercontent.com/Makrennel/hyprland-void/repository-x86_64-glibc'
 		alias cosmicbuild='sudo ./d77 -r /home/dani77/d77void-pkgs/hostdir/binpkgs/ -r /home/dani77/d77void-pkgs/hostdir/binpkgs/d77/ -r https://bellawagner.de/repo/x86_64 -b cosmic -- -T d77void'
